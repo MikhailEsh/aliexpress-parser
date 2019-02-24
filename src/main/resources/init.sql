0ce9e80a5b169c821e9bc73653ab7e7a63ffda3b
MikhailEsh <eshtokin.o@yandex.ru>
2019-02-24 22:44:08 +0300

    improve sql schema

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -2,7 +2,7 @@ create or replace table if not exists errors
 (
 	url_product text not null,
 	id int auto_increment,
-	url_group text not null,
+	url_group int default 1 not null,
 	constraint errors_id_uindex
 		unique (id)
 );
@@ -23,15 +23,17 @@ alter table links_group
 
 create or replace table if not exists links_products
 (
-	url_group text not null,
 	url_product text not null,
 	id varchar(255) not null,
 	url_feedback text not null,
 	count_feedback int not null,
 	count_ru_feedback int not null,
 	ratio int not null,
+	url_group int not null,
 	constraint links_products_id_uindex
-		unique (id)
+		unique (id),
+	constraint links_products_links_group_id_fk
+		foreign key (url_group) references links_group (id)
 );
 
 alter table links_products
