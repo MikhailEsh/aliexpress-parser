2946b6efc560068d1f1912974714eb1ef48e97ca
MikhailEsh <eshtokin.o@yandex.ru>
2019-02-24 14:30:55 +0300

    add new sql schema

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,3 +1,15 @@
+create or replace table if not exists errors
+(
+	url_product text not null,
+	id int auto_increment,
+	url_group text not null,
+	constraint errors_id_uindex
+		unique (id)
+);
+
+alter table errors
+	add primary key (id);
+
 create or replace table if not exists links_group
 (
 	url_group text not null,
@@ -9,18 +21,19 @@ create or replace table if not exists links_group
 alter table links_group
 	add primary key (id);
 
-
 create or replace table if not exists links_products
 (
 	url_group text not null,
 	url_product text not null,
-	id int auto_increment,
+	id varchar(255) not null,
+	url_feedback text not null,
+	count_feedback int not null,
+	count_ru_feedback int not null,
+	ratio int not null,
 	constraint links_products_id_uindex
 		unique (id)
 );
 
 alter table links_products
 	add primary key (id);
-
-
 
